505d33b5dfb3cd0ef6c1514c53e4e00d418e37c3
Morainy <morainchen135@gmail.com>
2015-04-23 23:23:25 +0800

    add code

---
+++ 6zigzigConversion.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  6zigzigConversion.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年04月23日 23时08分55秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution{
+	public:
+		string convert(string s , int numRows);
+};
+
+string Solution::convert(string s , int numRows)
+{
+
+}
+
+int main(int argc ,char * argv[])
+{
+
+}
