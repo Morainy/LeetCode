2ff27f109695ada5afbd53674648cd2e4694a1bb
Morainy <morainchen135@gmail.com>
2015-04-24 15:29:09 +0800

    add code

--- 6zigzigConversion.cpp
+++ 6zigzigConversion.cpp
@@ -26,10 +26,39 @@ class Solution{
 
 string Solution::convert(string s , int numRows)
 {
-
+	int length = s.length();
+	if(length <= 1 || numRows <= 1){
+		return s;
+	}
+	int circle = 2*numRows - 2;
+	int count = length / circle;
+	int mod = length % circle;
+	string res;
+	int distance = 2*numRows -2;	
+	int i , j;
+	for(i = 0 ; i < numRows ; i++){
+		for(j = 0 ; j <count ; j++){
+			//res.append(1 , s.at(i + j));
+			res.append(1 , s.at(i + j*distance));
+			if(i != 0 && i != numRows-1){
+				res.append(1 , s.at(i + j * distance + circle));
+			}
+		}
+		if(i < mod){
+			res.append(1 , s.at(i + j*distance));
+			if( i >= 2*numRows - mod - 1 && i!= numRows -1){
+				res.append(1 , s.at(i + j*distance + circle));
+			}
+		}
+		circle -= 2;
+	}
+	return res;
 }
 
 int main(int argc ,char * argv[])
 {
-
+	string str("ABCDE");
+	Solution sol;
+	string res = sol.convert(str , 4);
+	cout<<"res = " << res<<endl;
 }
