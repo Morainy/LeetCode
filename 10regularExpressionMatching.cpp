678ae8492f97d6c3acc069efbaa158e496a32934
Morainy <morainchen135@gmail.com>
2015-05-05 09:29:39 +0800

    add code

--- 10regularExpressionMatching.cpp
+++ 10regularExpressionMatching.cpp
@@ -32,10 +32,10 @@ bool Solution::isMatch(string s , string p)
 	for(i = 0 , j = 0 ; i < pLen ; i++){
 		if(p.at(i) == '.'){
 			if(i+1 < pLen && p.at(i + 1) == '*'){
-				j++;
-				while(j < sLen && s.at(j) == s.at(j+1)){
-					j++;
+				if(p.substr(i + 2 , p.length() - (i+2)).length() <= 0){
+					return true;
 				}
+				return isMatch(s , p.substr(i + 2 , p.length() - (i+2)));
 			}
 			else{
 				j++;
@@ -47,16 +47,30 @@ bool Solution::isMatch(string s , string p)
 					j++;
 				}
 				i++;
+				if(j >= sLen){
+					int start = s.length() - (p.length() -(i + 1));
+					int length = p.length() - (i +1);
+					return isMatch(s.substr(start , length) , p.substr(i+1 , p.length() -(i+1)));
+				}
 			}
 			else{
-				j++;
+				if(j < sLen && p.at(i) == s.at(j)){
+					j++;
+				}
+				else{
+					return false;
+				}
 			}
 		}
 	}
+	if(j < sLen || i < pLen){
+		return false;
+	}
+	return true;
 }
 int main(int argc ,char *argv[])
 {
-	string str1("aab") , str2("c*a*b");
+	string str1("aaa") , str2("a*a");
 	Solution sol;
 	bool res;
 	res = sol.isMatch(str1 , str2);
