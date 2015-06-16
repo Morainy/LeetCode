03d35a482997a354df1175e45c168dfffbe92a6f
Morainy <morainchen135@gmail.com>
2015-06-16 22:31:10 +0800

    add code

--- 78subsets.cpp
+++ 78subsets.cpp
@@ -1,12 +1,12 @@
 /*
  * =====================================================================================
  *
- *       Filename:  77combine.cpp
+ *       Filename:  subsets.cpp
  *
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  2015年06月16日 20时01分49秒
+ *        Created:  2015年06月16日 21时57分51秒
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -15,18 +15,24 @@
  *
  * =====================================================================================
  */
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution{
 	public:
-		vector<vector<int> > combine(int n , int k);
-		void solve(vector<vector<int> > &res , vector<int>& tmp ,  vector<int>& nums , int begin , int k);
+		vector<vector<int> > subsets(vector<int>& nums);
+		void solve(vector<vector<int> >& res , vector<int>& tmp , vector<int>& nums , int begin , int k);
 };
 
-void Solution::solve(vector<vector<int> > &res ,vector<int>& tmp ,  vector<int>& nums , int begin , int k)
+void Solution::solve(vector<vector<int> >& res , vector<int>& tmp , vector<int>& nums , int begin , int k)
 {
+	if(k == 0){
+		res.push_back(tmp);
+		return;
+	}
 	if(k == 1){
 		for(int i = begin ; i < nums.size() ; i++){
 			tmp.push_back(nums[i]);
@@ -35,32 +41,37 @@ void Solution::solve(vector<vector<int> > &res ,vector<int>& tmp ,  vector<int>&
 		}
 		return;
 	}
-	for(int j = begin ; j <= nums.size() -k ; j++){
+	for(int j = begin ; j <= nums.size()-k ; j++){
 		tmp.push_back(nums[j]);
 		vector<int> anotherTmp(tmp);
-		solve(res , anotherTmp , nums , j + 1 , k -1);
+		solve(res , anotherTmp , nums , j+1 , k-1);
 		tmp.pop_back();
 	}
 }
-
-vector<vector<int> > Solution::combine(int n , int k)
+vector<vector<int> > Solution::subsets(vector<int>& nums)
 {
 	vector<vector<int> > res;
-	vector<int> nums;
-	for(int i = 1 ; i <= n ; i++){
-		nums.push_back(i);
-	}
 	vector<int> tmp;
-	solve(res , tmp , nums , 0 , k);
+	sort(nums.begin() , nums.end());
+	nums.erase(unique(nums.begin() , nums.end()) , nums.end());
+	for(int i = 0 ; i <= nums.size() ; i++){
+		solve(res , tmp , nums , 0 , i);
+	}
 	return res;
 }
 
 int main(int argc , char* argv[])
 {
 	Solution sol;
-	int n = 4 , k = 2;
 	vector<vector<int> > res;
-	res = sol.combine(n , k);
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(2);
+	nums.push_back(3);
+
+	res = sol.subsets(nums);
+
 	for(int i = 0 ; i < res.size() ; i++){
 		for(int j = 0 ; j < res[i].size() ; j++){
 			cout<<res[i][j]<<" ";
